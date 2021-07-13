9b389c54cd73fe0adb339b3d7034def7618b1348
jah083 <jah083@bucknell.edu>
2021-07-13 17:42:00 -0400

    d53

--- Database.py
+++ Database.py
@@ -331,12 +331,12 @@ class Database:
                 connection = self._connect()
 
             query = ("CALL log_access_completion(%s, %s)")
-            query2 = ("SELECT id FROM log")
+            query2 = ("SELECT id FROM log WHERE id > 49000")
 
             logging.debug("CALL log_access_completion({}, {})".format(card_id, equipment_id))
             cursor = connection.cursor()
             cursor.execute(query2)
-            logging.debug(cursor.fetchone())
+            logging.debug(cursor.fetchall())
 
             cursor.execute(query, (card_id, equipment_id))
 
