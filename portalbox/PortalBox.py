b1ac9972f5ee8dbca5462fe313526d77e22a3849
jah083 <jah083@bucknell.edu>
2021-07-13 15:44:13 -0400

    d29

--- portalbox/PortalBox.py
+++ portalbox/PortalBox.py
@@ -337,7 +337,7 @@ class PortalBox:
         self.beep_signal = True
         while(self.beep_signal):
             self.set_buzzer(True)
-            sleep(.2)
+            sleep(.1)
             self.set_buzzer(False)
             sleep(1/rate)
         self.set_buzzer(False)
