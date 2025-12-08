d26f32cf5132221155d827d5da786365d40db5e6
Tom Egan <tom@tomegan.tech>
2025-12-08 15:53:49 -0500

    feature: rough in begin_usage_session and end_usage_session

--- WebServiceClient.py
+++ WebServiceClient.py
@@ -1,5 +1,13 @@
 import requests
 
+class NotAuthorizedError(Exception):
+    """
+    Exception to raise when the web service reports the user represented by a card
+    is not permitted to activate the equipment
+    """
+
+    pass
+
 class NotRegisteredError(Exception):
     """
     Exception to raise when the web service reports a failure because the
@@ -54,3 +62,37 @@ class Client:
 
         if response.status_code == 409:
             raise OutOfServiceError()
+
+    def begin_usage_session(self, card_id: str):
+        """Request that the service begin a usage session for the user associated with the card
+
+        Raises
+        ------
+        NotAuthorizedError
+            if the user represented by the card can not use the attached equipment
+        """
+
+        url = f"{self.url}/api/v2/box-activation.php"
+        params = {"mac": self.mac}
+        headers = request_headers = {'Authorization': f'Bearer {card_id}'}
+        response = requests.put(url, params = params, headers = headers)
+
+        if response.status_code in (401, 403):
+            raise NotAuthorizedError()
+
+    def end_usage_session(self, card_id: str):
+        """Tell the backend that the equipment session has ended
+
+        Raises
+        ------
+        NotAuthorizedError
+            if the card was invalid for ending the usage session
+        """
+
+        url = f"{self.url}/api/v2/box-activation.php"
+        params = {"mac": self.mac}
+        headers = request_headers = {'Authorization': f'Bearer {card_id}'}
+        response = requests.post(url, params = params, headers = headers)
+
+        if response.status_code in (401, 403):
+            raise NotAuthorizedError()
